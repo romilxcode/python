6b31ed4bce891bb242ceeef42d17cff620e1af4f
romilxcode <romilxcode@gmail.com>
2025-12-16 19:50:49 +0530

    Add tuple marks practice in Chapter 5

---
+++ Chapter 5/practice3.py
@@ -0,0 +1,12 @@
+#Create a dictionary storing meanings of 3 English Words.
+
+engWords = {
+    "firstWord": "Therapy",
+    "secondWord": "is",
+    "thirdWord": "worth it",
+}
+
+print(type(engWords))
+print(engWords["firstWord"])
+print(engWords["secondWord"])
+print(engWords["thirdWord"])
