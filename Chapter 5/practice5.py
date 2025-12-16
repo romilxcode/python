c52a97150319e25dd617fa0316cba2f16923c542
romilxcode <romilxcode@gmail.com>
2025-12-16 19:43:08 +0530

    Delete Chapter 5 directory

--- Chapter 5/practice5.py
+++
@@ -1,8 +0,0 @@
-#Try to add both integer 9 and float 9.0 to a set and observe the result.
-
-numberSet = set()
-numberSet.add(9)
-numberSet.add(9.0)
-print("Set after adding 9 and 9.0:", numberSet)
-
-# Observation: Both 9 and 9.0 are considered the same in a set, as sets do not allow duplicate values.
