e5e3814008c2d6fde3bfd1894cba074e574bfe13
romilxcode <romilxcode@gmail.com>
2025-12-16 19:40:16 +0530

    python

--- Chapter 5/dictBasics.py
+++ Chapter 5/dictBasics.py
@@ -4,10 +4,19 @@ student= {
     "name": "Romil Unadkat",
     "city": "Amreli",
     "age": 27,
-    "rollNumber": 69
+    "rollNumber": 69,
 }
 
 print(type(student))
 print(student["name"])
 print(student)
 print(student["city"])
+# student["city"]= "Bengaluru"
+# print(student)
+student["favSubject"]= "Python"
+print(student)
+student.pop("favSubject")
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items())
